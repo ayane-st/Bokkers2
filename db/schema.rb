e28ecd431f09833852c2afdcb472029a75549d02
EC2 Default User <ec2-user@ip-172-31-11-36.ap-northeast-1.compute.internal>
2025-02-19 15:23:09 +0000

    コントローラーの作成

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2025_02_13_143446) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.string "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
