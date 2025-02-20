157535892bbe414bac45fb14d3e072c6a86b931d
EC2 Default User <ec2-user@ip-172-31-11-36.ap-northeast-1.compute.internal>
2025-02-20 13:06:34 +0000

    Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :books, only: [:new, :create, :index, :show, :edit]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:index, :show, :edit]
   get 'homes/top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
