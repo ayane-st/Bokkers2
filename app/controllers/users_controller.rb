b79a3ee41323c9e180a62286cf538410d8fbfece
EC2 Default User <ec2-user@ip-172-31-11-36.ap-northeast-1.compute.internal>
2025-02-25 17:00:43 +0000

    balidation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.save(user_params)
+      flash[:notice]="You have created user successfully."
+      redirect_to user_path(user.id)
+    else
+      flash[:notice]="error"
+      render :new
+    end
+  end
+
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -11,8 +27,13 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    if user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(user.id)
+    else
+      flash[:notice]="error"
+      redirect_to user_path(user.id)
+    end
   end
 
   def index
@@ -25,12 +46,14 @@ class UsersController < ApplicationController
   def create_book
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
-    #if @book.save
-      # 保存に成功した場合の処理
-    #else
-      #保存に失敗した場合の処理
+    if @book.save
+      flash[:notice]="You have created book successfully."
+      redirect_to books_path
+    else
+      flash[:notice]="error"
+      redirect_to books_path
+    end
+    
   end
 
   private
